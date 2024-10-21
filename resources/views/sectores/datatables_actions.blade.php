47392189b0db8e1a229c51ffd0318e65eba1aae2
Ricesc <ricesc14@gmail.com>
2024-10-21 17:39:14 -0300

    correción datatables actions

--- resources/views/sectores/datatables_actions.blade.php
+++ resources/views/sectores/datatables_actions.blade.php
@@ -1,28 +1,3 @@
-{!! Form::open(['route' => ['sectores.destroy', $sect_id], 'method' => 'delete']) !!}
-<div class='btn-group'>
-    <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Actions
-    </button>
-    <div class="dropdown-menu">
-        <a href="{{ route('sectores.show', $sect_id) }}" class='dropdown-item'>
-            <i class="fa fa-eye"></i> Ver
-        </a>
-        <a href="{{ route('sectores.edit', $sect_id) }}" class='dropdown-item'>
-            <i class="fa fa-edit"></i> Editar
-        </a>
-        <div class="dropdown-divider"></div>
-        {!! Form::button('<i class="fa fa-trash"></i> Borrar', [
-            'type' => 'submit',
-            'class' => 'dropdown-item text-danger',
-            'onclick' => "return confirm('Are you sure?')"
-        ]) !!}
-    </div>
-</div>
-{!! Form::close() !!}
-
-
-{{-- wweweweweweweewew --}}
-
 {!! Form::open(['route' => ['sectores.destroy', $sect_id], 'method' => 'delete', 'class' => 'd-inline', 'id' => 'delete-form-' . $sect_id]) !!}
 <div class='btn-group'>
     <button type="button" class="btn btn-default btn-xs dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
