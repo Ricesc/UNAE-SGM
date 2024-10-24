efd31cad0ef0836ce0408389982adf90e527aa1d
Enzo Ortiz Tilleria <87786114+esfot26@users.noreply.github.com>
2024-10-24 16:06:08 -0300

    Update index.blade.php

--- resources/views/bienes/index.blade.php
+++ resources/views/bienes/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-sm-6">
                     <a class="btn btn-primary float-right"
                        href="{{ route('bienes.create') }}">
-                        Add New
+                        Agregar
                     </a>
                 </div>
             </div>
@@ -34,6 +34,26 @@
                 </div>
             </div>
 
+        </div>
+    </div>
+
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">Confirmar Eliminación</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    ¿Estás seguro de que deseas eliminar esta baja?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="button" class="btn btn-danger" id="confirmDelete">Borrar</button>
+                </div>
+            </div>
         </div>
     </div>
 
